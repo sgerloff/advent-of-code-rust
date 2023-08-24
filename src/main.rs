58acd64fc680610885bc6afacf39396fe7df9bb1
Dr. Sascha Gerloff <sascha.gerloff@talentwunder.com>
2023-08-24 11:56:50 +0200

    refactor to solve part 2 of first task

--- src/main.rs
+++ src/main.rs
@@ -9,40 +9,67 @@ where P: AsRef<Path> {
     Ok(io::BufReader::new(file).lines())
 }
 
-fn find_largest_group(lines: io::Lines<io::BufReader<File>>) -> usize {
-    let mut current_sum: usize = 0;
-    let mut max_sum: usize = 0;
+struct TopNGroupCounter {
+    top_n: usize,
+    _current_sum: usize,
+    _top_sums: Vec::<usize>
+}
+
+impl Default for TopNGroupCounter {
+    fn default() -> TopNGroupCounter {
+        TopNGroupCounter { _current_sum: 0, top_n: 1, _top_sums: Vec::<usize>::new() }
+    }
+}
+
+impl TopNGroupCounter {
+    fn add(&mut self, number: usize) {
+        self._current_sum = self._current_sum + number;
+    }
+
+    fn complete(&mut self) {
+        self._top_sums.push(self._current_sum);
+        self._top_sums.sort();
+        self._top_sums.reverse();
+        self._top_sums.truncate(self.top_n);
+        self.reset();
+    }
+
+    fn reset(&mut self) {
+        self._current_sum = 0;
+    }
+}
+
+
+fn apply_top_n_group_counter(line: String, counter: &mut TopNGroupCounter) {
+    let number = line.parse::<usize>();
+    match number {
+        Ok(number) => counter.add(number),
+        Err(_) => counter.complete()
+    }
+}
+
+
+fn process_file(lines: io::Lines<io::BufReader<File>>, top_n: usize) -> Vec<usize> {
+    let mut counter = TopNGroupCounter {top_n: top_n, ..Default::default()};
     for line in lines {
         match line {
-            Ok(line) => {
-                let number = line.parse::<usize>();
-                match number {
-                    Ok(number) => current_sum += number,
-                    Err(_) => {
-                        if current_sum > max_sum {
-                            max_sum = current_sum;
-                        };
-                        current_sum = 0;
-                    }
-                }
-            },
+            Ok(line) => apply_top_n_group_counter(line, &mut counter),
             Err(line) => println!("{line}")
         }
     }
-    max_sum
+    return counter._top_sums;
 }
-
 
 fn main() {
     println!("Read file...");
     let sample_path = String::from("/home/sascha/Documents/AdventOfCode/2022/01/sample.txt");
-
-
     let lines = stream_lines(&sample_path);
     match lines {
         Ok(lines) => {
-            let max_sum = find_largest_group(lines);
-            println!("{max_sum}");
+            let max_sums = process_file(lines, 3);
+            for _sum in max_sums {
+                println!("{_sum}");
+            }
         },
         Err(_) => println!("File '{sample_path}' does not exist!")
     }
