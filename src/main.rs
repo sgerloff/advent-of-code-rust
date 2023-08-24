3d547139285b5b15770058a105fb2d30df0a6a68
Dr. Sascha Gerloff <sascha.gerloff@talentwunder.com>
2023-08-24 14:05:37 +0200

    move code to seperate file

--- src/main.rs
+++ src/main.rs
@@ -2,43 +2,16 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
+use top_n_counter::TopNGroupCounter;
+
+pub mod top_n_counter;
+
 
 fn stream_lines<P> (filename: P) -> io::Result<io::Lines<io::BufReader<File>>> 
 where P: AsRef<Path> {
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
-
-struct TopNGroupCounter {
-    top_n: usize,
-    _current_sum: usize,
-    _top_sums: Vec::<usize>
-}
-
-impl Default for TopNGroupCounter {
-    fn default() -> TopNGroupCounter {
-        TopNGroupCounter { _current_sum: 0, top_n: 1, _top_sums: Vec::<usize>::new() }
-    }
-}
-
-impl TopNGroupCounter {
-    fn add(&mut self, number: usize) {
-        self._current_sum = self._current_sum + number;
-    }
-
-    fn complete(&mut self) {
-        self._top_sums.push(self._current_sum);
-        self._top_sums.sort();
-        self._top_sums.reverse();
-        self._top_sums.truncate(self.top_n);
-        self.reset();
-    }
-
-    fn reset(&mut self) {
-        self._current_sum = 0;
-    }
-}
-
 
 fn apply_top_n_group_counter(line: String, counter: &mut TopNGroupCounter) {
     let number = line.parse::<usize>();
@@ -50,14 +23,14 @@ fn apply_top_n_group_counter(line: String, counter: &mut TopNGroupCounter) {
 
 
 fn process_file(lines: io::Lines<io::BufReader<File>>, top_n: usize) -> Vec<usize> {
-    let mut counter = TopNGroupCounter {top_n: top_n, ..Default::default()};
+    let mut counter = TopNGroupCounter::new(top_n);
     for line in lines {
         match line {
             Ok(line) => apply_top_n_group_counter(line, &mut counter),
             Err(line) => println!("{line}")
         }
     }
-    return counter._top_sums;
+    return counter.top_sums;
 }
 
 fn main() {
